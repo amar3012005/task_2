f3db46d2a69fece8559f42f2b3f5a6a8931e1100
amar3012005 <148769827+amar3012005@users.noreply.github.com>
2024-07-08 23:19:38 +0530

    Update straight_line_planner.cpp

--- src/custom_planner/src/straight_line_planner.cpp
+++ src/custom_planner/src/straight_line_planner.cpp
@@ -9,96 +9,93 @@ namespace custom_planner
 {
 
 void StraightLine::configure(
-  const rclcpp_lifecycle::LifecycleNode::WeakPtr & parent,
-  std::string name, std::shared_ptr<tf2_ros::Buffer> tf,
-  std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmap_ros)
+const rclcpp_lifecycle::LifecycleNode::WeakPtr & parent,
+std::string name, std::shared_ptr<tf2_ros::Buffer> tf,
+std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmap_ros)
 {
-  node_ = parent.lock();
-  name_ = name;
-  tf_ = tf;
-  costmap_ = costmap_ros->getCostmap();
-  global_frame_ = costmap_ros->getGlobalFrameID();
+node_ = parent.lock();
+name_ = name;
+tf_ = tf;
+costmap_ = costmap_ros->getCostmap();
+global_frame_ = costmap_ros->getGlobalFrameID();
 
-  // Parameter initialization
-  nav2_util::declare_parameter_if_not_declared(
-    node_, name_ + ".interpolation_resolution", rclcpp::ParameterValue(0.1));
-  node_->get_parameter(name_ + ".interpolation_resolution", interpolation_resolution_);
+
+nav2_util::declare_parameter_if_not_declared(
+node_, name_ + ".interpolation_resolution", rclcpp::ParameterValue(0.1));
+node_->get_parameter(name_ + ".interpolation_resolution", interpolation_resolution_);
 }
 
 void StraightLine::cleanup()
 {
-  RCLCPP_INFO(
-    node_->get_logger(), "Cleaning up plugin %s of type StraightLine",
-    name_.c_str());
+RCLCPP_INFO(
+node_->get_logger(), "Cleaning up plugin %s of type StraightLine",
+name_.c_str());
 }
 
 void StraightLine::activate()
 {
-  RCLCPP_INFO(
-    node_->get_logger(), "Activating plugin %s of type StraightLine",
-    name_.c_str());
+RCLCPP_INFO(
+node_->get_logger(), "Activating plugin %s of type StraightLine",
+name_.c_str());
 }
 
 void StraightLine::deactivate()
 {
-  RCLCPP_INFO(
-    node_->get_logger(), "Deactivating plugin %s of type StraightLine",
-    name_.c_str());
+RCLCPP_INFO(
+node_->get_logger(), "Deactivating plugin %s of type StraightLine",
+name_.c_str());
 }
 
 nav_msgs::msg::Path StraightLine::createPlan(
-  const geometry_msgs::msg::PoseStamped & start,
-  const geometry_msgs::msg::PoseStamped & goal)
+const geometry_msgs::msg::PoseStamped & start,
+const geometry_msgs::msg::PoseStamped & goal)
 {
-  nav_msgs::msg::Path global_path;
+nav_msgs::msg::Path global_path;
 
-  // Checking if the goal and start state is in the global frame
-  if (start.header.frame_id != global_frame_) {
-    RCLCPP_ERROR(
-      node_->get_logger(), "Planner will only accept start position from %s frame",
-      global_frame_.c_str());
-    return global_path;
-  }
-
-  if (goal.header.frame_id != global_frame_) {
-    RCLCPP_ERROR(
-      node_->get_logger(), "Planner will only accept goal position from %s frame",
-      global_frame_.c_str());
-    return global_path;
-  }
-
-  global_path.poses.clear();
-  global_path.header.stamp = node_->now();
-  global_path.header.frame_id = global_frame_;
-
-  // Calculate the number of segments based on interpolation resolution
-  double dist = std::hypot(
-    goal.pose.position.x - start.pose.position.x,
-    goal.pose.position.y - start.pose.position.y);
-  int num_segments = std::ceil(dist / interpolation_resolution_);
-
-  double x_increment = (goal.pose.position.x - start.pose.position.x) / num_segments;
-  double y_increment = (goal.pose.position.y - start.pose.position.y) / num_segments;
-
-  // Create poses along the straight line path
-  for (int i = 0; i <= num_segments; ++i) {
-    geometry_msgs::msg::PoseStamped pose;
-    pose.pose.position.x = start.pose.position.x + x_increment * i;
-    pose.pose.position.y = start.pose.position.y + y_increment * i;
-    pose.pose.position.z = 0.0;
-    pose.pose.orientation.x = 0.0;
-    pose.pose.orientation.y = 0.0;
-    pose.pose.orientation.z = 0.0;
-    pose.pose.orientation.w = 1.0;
-    pose.header.stamp = node_->now();
-    pose.header.frame_id = global_frame_;
-    global_path.poses.push_back(pose);
-  }
-
-  return global_path;
+// Checking if the goal and start state is in the global frame
+if (start.header.frame_id != global_frame_) {
+RCLCPP_ERROR(
+node_->get_logger(), "Planner will only accept start position from %s frame",
+global_frame_.c_str());
+return global_path;
 }
 
-}  // namespace custom_planner
+if (goal.header.frame_id != global_frame_) {
+RCLCPP_ERROR(
+node_->get_logger(), "Planner will only accept goal position from %s frame",
+global_frame_.c_str());
+return global_path;
+}
+
+global_path.poses.clear();
+global_path.header.stamp = node_->now();
+global_path.header.frame_id = global_frame_;
+
+
+double dist = std::hypot(
+goal.pose.position.x - start.pose.position.x,
+goal.pose.position.y - start.pose.position.y);
+int num_segments = std::ceil(dist / interpolation_resolution_);
+
+double x_increment = (goal.pose.position.x - start.pose.position.x) / num_segments;
+double y_increment = (goal.pose.position.y - start.pose.position.y) / num_segments;
+
+for (int i = 0; i <= num_segments; ++i) {
+geometry_msgs::msg::PoseStamped pose;
+pose.pose.position.x = start.pose.position.x + x_increment * i;
+pose.pose.position.y = start.pose.position.y + y_increment * i;
+pose.pose.position.z = 0.0;
+pose.pose.orientation.x = 0.0;
+pose.pose.orientation.y = 0.0;
+pose.pose.orientation.z = 0.0;
+pose.pose.orientation.w = 1.0;
+pose.header.stamp = node_->now();
+pose.header.frame_id = global_frame_;
+global_path.poses.push_back(pose);
+}
+}
+}
+// namespace custom_planner
 
 #include "pluginlib/class_list_macros.hpp"
 PLUGINLIB_EXPORT_CLASS(custom_planner::StraightLine, nav2_core::GlobalPlanner)
